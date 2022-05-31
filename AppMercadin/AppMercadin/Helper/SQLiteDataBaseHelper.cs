7404a7cb37e40082ea6225df2456aa61a989a72c
raph <Usuario@CPD02-M13>
2022-05-31 12:11:43 -0300

    Editei foi tudo, ufa

---
+++ AppMercadin/AppMercadin/Helper/SQLiteDataBaseHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+using AppMercadin.Model;
+using System.Threading.Tasks;
+
+namespace AppMercadin.Helper
+{
+    public class SQLiteDataBaseHelper
+    {
+        readonly SQLiteAsyncConnection _connection;
+        public SQLiteDataBaseHelper(string path)
+        {
+            _connection = new SQLiteAsyncConnection(path);
+            _connection.CreateTableAsync<Tarefa>().Wait();
+        }
+
+        public Task<int> Save(Tarefa t)
+        {
+            return _connection.InsertAsync(t);
+        }
+
+        public Task<List<Tarefa>> GetAllRows()
+        {
+            return _connection.Table<Tarefa>().ToListAsync();
+        }
+
+        public Task<int> Delete(int id)
+        {
+            string sql = "DELETE tarefa SET " +
+                "Descricao=?, Data_criacao=?, Data_conclusao=? " +
+                "WHERE Id=?";
+            return _connection.Table<Tarefa>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Tarefa>> Update(Tarefa t)
+        {
+            string sql = "UPDATE tarefa SET " +
+                         "Descricao=?, Data_criacao=?, Data_conclusao=? " +
+                         "WHERE Id=?";
+
+            return _connection.QueryAsync<Tarefa>(sql,
+                t.Produto, t.Preco, t.Data_Criacao, t.Data_Conclusao, t.Id);
+        }
+
+        public Task<List<Tarefa>> Search(string q)
+        {
+            string sql = "SELECT * FROM tarefa WHERE Descricao LIKE '%" + q + "%'";
+
+            return _connection.QueryAsync<Tarefa>(sql);
+        }
+
+    }
+}
---
+++ AppMercadin/AppMercadin/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AppMercadin.Model;
+
+namespace AppMercadin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Listagem());
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Tarefa t = new Tarefa();
+            t.Produto = txt_produto.Text;
+            t.Preco = txt_preco.Text;
+            t.Data_Criacao = dtpck_data_criacao.Date;
+
+            await App.Database.Save(t);
+
+            await DisplayAlert("Booa", "Tudo Salvo no SQLite", "OK");
+
+            await Navigation.PushAsync(new Listagem());
+        }
+    }
+}
